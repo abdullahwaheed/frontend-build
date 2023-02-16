f1927a3f19c08a30f97e03fff6c5109602dc40d3
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2023-02-16 14:47:40 +0500

    refactor: added test configurations

--- config/babel.config.js
+++ config/babel.config.js
@@ -34,6 +34,10 @@ module.exports = {
       plugins: [
         [
           'formatjs',
+          {
+            idInterpolationPattern: '[sha512:contenthash:base64:6]',
+            ast: true,
+          },
         ],
       ],
     },
